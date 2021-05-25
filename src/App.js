856b1e2ed9d5c208ea5e5a74df67be46ceb41d9a
Richard Bagdazian <rbagdazian@gmail.com>
2021-05-25 20:25:05 +0000

    first phase of support for image transfer to s3

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import FileUpload from './FileUpload';
 import FileDisplay from './FileDisplay';
 
 import './App.css';
+import Amplify, { Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 
 function App() {
   const [curState, setCurState] = useState({valid:false, fileInfo:[]});
+  const [loading, setLoading] = useState(false);  
+  
+  const downloadUrl = async () => {
+    // Creates download url that expires in 5 minutes/ 300 seconds
+    const downloadUrl = await Storage.get('picture.jpg', { expires: 300 });
+    window.location.href = downloadUrl
+  }
+  
+  
   return (
     <div className="App">
       <header className="App-header">
