3ad45bb60a2732ed96f2d9c2861799bdd12da60d
Richard Bagdazian <rbagdazian@gmail.com>
2021-06-02 17:07:10 +0000

    added boto3 and call to find buckets

--- amplify/backend/function/imgprocLambdaFunc/src/imgifce.py
+++ amplify/backend/function/imgprocLambdaFunc/src/imgifce.py
@@ -1,5 +1,13 @@
-
+import boto3
 
 def imghandler(pqs):
-    responseMsg = 'Hello from imghandler!'
+    # Let's use Amazon S3
+    s3 = boto3.resource('s3')
+    
+    # Print out bucket names
+    bucketList = list()
+    for bucket in s3.buckets.all():
+        bucketList.append(bucket)
+    
+    responseMsg = 'Hello from imghandler!' + repr(bucketList);
     return responseMsg
